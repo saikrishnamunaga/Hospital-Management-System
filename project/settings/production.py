631302ae56753547c6089e0d8e86d5eac2bc657a
saikrishnamunaga <mmunagasaikrishna@gmail.com>
2026-02-26 11:14:01 +0530

    Fix ALLOWED_HOSTS in production settings

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,6 +1,9 @@
 from .base import *
+import os
+
 DEBUG = False
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'hospital-management-system-rfkq.onrender.com').split(',')
+
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
