a5666273041a9e0e1e18fd491d1fb90b317d9f9b
saikrishnamunaga <mmunagasaikrishna@gmail.com>
2026-02-27 09:56:54 +0530

    Update CORS and ALLOWED_HOSTS for local development

--- project/settings/base.py
+++ project/settings/base.py
@@ -90,7 +90,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'dist']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:3001,http://127.0.0.1:3001').split(',')
 CORS_ALLOW_CREDENTIALS = True
 
 REDIS_URL = os.environ.get('REDIS_URL')
--- project/settings/development.py
+++ project/settings/development.py
@@ -1,3 +1,3 @@
 from .base import *
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
